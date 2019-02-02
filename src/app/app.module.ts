465ebd5a4eee4a7069b434671b96e9e9d6daabfb
tiboxman <tibogrouet@hotmail.fr>
2019-02-02 03:09:36 +0100

    flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+
 import { AppComponent } from './app.component';
 import { CommingSoonComponent } from './pages/comming-soon/comming-soon.component';
 import { HomeComponent } from './pages/comming-soon/home/home.component';
